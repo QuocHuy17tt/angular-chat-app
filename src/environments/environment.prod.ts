c877ec5c99a58ab86973cb017f4968aa783de74d
unknown <huynq102@viettel.com.vn>
2022-07-07 15:00:32 +0700

    create-project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'message-application-huynq102',
+    appId: '1:581211207929:web:39f58152479f6897c0ca28',
+    storageBucket: 'message-application-huynq102.appspot.com',
+    apiKey: 'AIzaSyCkpJ-FwuJnfUlnxvNvQgNF_IBy58Cv3HY',
+    authDomain: 'message-application-huynq102.firebaseapp.com',
+    messagingSenderId: '581211207929',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'message-application-huynq102',
+    appId: '1:581211207929:web:39f58152479f6897c0ca28',
+    storageBucket: 'message-application-huynq102.appspot.com',
+    apiKey: 'AIzaSyCkpJ-FwuJnfUlnxvNvQgNF_IBy58Cv3HY',
+    authDomain: 'message-application-huynq102.firebaseapp.com',
+    messagingSenderId: '581211207929',
+  },
+  production: false,
+  apiUrl: '',
+  stream: {
+    key: ''
+  }
 };
 
 /*
